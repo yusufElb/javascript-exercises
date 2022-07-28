85ee16dbc6c588f0016a9e0444bc1129cef75130
yusufElb <yusuf.elbourki@gmail.com>
2022-07-28 22:29:55 +0100

    jira unit tests and js exercises
    tracking progress of unit tested js exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+// const removeFromArray = function(array, args) {
+//     let index = array.indexOf(args);
+//     if (index > -1){
+//       array.splice(index, 1);
+//     }
+//     return array;
+// };
 
+const removeFromArray = function (...args) {
+  const array = args[0];
+  const newArray = [];
+  array.forEach((item) => {
+    if(!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
+
+// const removeFromArray = function (...args) {
+//   let array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+  if (!Number.isInteger(intOne) || !Number.isInteger(intTwo)) return "ERROR";
+  if (intOne < 0 || intTwo < 0) return "ERROR";
+  
+  if (intOne > intTwo) {
+    const temp = intOne;
+    intOne = intTwo;
+    intTwo = temp;
+  }
 
+  let result = 0
+    for(let i = intOne; i < intTwo + 1; i++){
+      result+=i
+    }
+    return result
 };
 
 // Do not edit below this line
